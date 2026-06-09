54ab3b3d25ef5158d32718983e39659ecb98d4f0
Wellington-Miguel <wellington2010_silva@hotmail.com>
2026-06-08 21:02:23 -0300

    mudança de output

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -7,3 +7,8 @@ output "asg_arn" {
   description = "ARN do Auto Scaling Group"
   value       = module.asg.asg_arn
 }
+
+output "api_url" {
+  description = "URL pública para acessar a API"
+  value       = "http://${module.alb.alb_dns_name}"
+}
